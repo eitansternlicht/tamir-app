c04ecb9f74b9bef2618c123fd1b29ed156de03c5
Eitan Sternlicht <eitanoffun@gmail.com>
2019-04-24 01:36:56 +0300

    added DateTimePicker functionality to AttendanceTab

--- src/scenes/scenesections/AttendanceTab.js
+++ src/scenes/scenesections/AttendanceTab.js
@@ -1,54 +1,127 @@
 import React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { Container, Button, Text, Icon } from 'native-base';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import { toClockTime } from '../../utils/date-utils';
 
 const activities = [
   { shortTitle: 'פעילות א׳', key: 'a' },
   { shortTitle: 'פעילות ב׳', key: 'b' },
   { shortTitle: 'פעילות ג׳', key: 'c' }
 ];
-const AttendanceTab = () => {
-  return (
-    <Container style={styles.container}>
-      <Container style={styles.section}>
-        <Button>
-          <Icon name="calendar" />
-          <Text>עריכת נוכחות</Text>
-        </Button>
-      </Container>
-      <Container style={styles.section}>
-        <Icon name="create" style={{ fontSize: 20, color: 'blue', paddingTop: 5 }} />
-        <Text style={styles.time}>08:00</Text>
-        <Button>
-          <Text>כניסה</Text>
-        </Button>
-      </Container>
-      <Container style={styles.section}>
-        <Text style={styles.time}>--:--</Text>
-        <Button>
-          <Text>יציאה</Text>
-        </Button>
-      </Container>
+class AttendanceTab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showStartTimePicker = this.showStartTimePicker.bind(this);
+    this.showEndTimePicker = this.showEndTimePicker.bind(this);
+    this.hideEndTimePicker = this.hideEndTimePicker.bind(this);
+    this.hideStartTimePicker = this.hideStartTimePicker.bind(this);
+    this.handleStartTimePicked = this.handleStartTimePicked.bind(this);
+    this.handleEndTimePicked = this.handleEndTimePicked.bind(this);
+    this.state = {
+      startTime: null,
+      endTime: null,
+      startTimePickerOpen: false,
+      endTimePickerOpen: false
+    };
+  }
 
-      <Container style={[styles.section, { padding: 0, paddingTop: 0 }]}>
-        <Text style={styles.activities}>פעילויות</Text>
-        <Icon name="arrow-dropdown" />
-      </Container>
-      <FlatList data={activities} renderItem={({ item }) => <Text>{item.shortTitle}</Text>} />
+  showStartTimePicker() {
+    this.setState({ startTimePickerOpen: true });
+  }
 
-      <Container style={[styles.section, { padding: 0, paddingTop: 0 }]}>
-        <Text style={styles.activities}>הוסף פעילות</Text>
-        <Icon name="add-circle" />
-      </Container>
+  showEndTimePicker() {
+    this.setState({ endTimePickerOpen: true });
+  }
 
-      <Container style={[styles.section, { justifyContent: 'center' }]}>
-        <Button>
-          <Text>שמור</Text>
-        </Button>
+  hideStartTimePicker() {
+    this.setState({ startTimePickerOpen: false });
+  }
+
+  hideEndTimePicker() {
+    this.setState({ endTimePickerOpen: false });
+  }
+
+  handleStartTimePicked(time) {
+    this.setState({ startTime: time, startTimePickerOpen: false });
+  }
+
+  handleEndTimePicked(time) {
+    this.setState({ endTime: time, endTimePickerOpen: false });
+  }
+
+  renderTime({ startTime, endTime }) {
+    const time = startTime || endTime;
+    if (time) {
+      return (
+        <TouchableOpacity
+          style={{ flexDirection: 'row' }}
+          onPress={startTime ? this.showStartTimePicker : this.showEndTimePicker}>
+          <Icon
+            name="create"
+            style={{ fontSize: 20, color: 'blue', paddingTop: 5, paddingRight: 5 }}
+          />
+          <Text style={styles.time}>{toClockTime(time)}</Text>
+        </TouchableOpacity>
+      );
+    }
+    return <Text style={styles.time}>{toClockTime(time)}</Text>;
+  }
+
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Container style={styles.section}>
+          <Button>
+            <Icon name="calendar" />
+            <Text>עריכת נוכחות</Text>
+          </Button>
+        </Container>
+        <Container style={styles.section}>
+          {this.renderTime({ startTime: this.state.startTime })}
+          <Button onPress={this.showStartTimePicker}>
+            <Text>כניסה</Text>
+          </Button>
+        </Container>
+        <Container style={styles.section}>
+          {this.renderTime({ endTime: this.state.endTime })}
+          <Button onPress={this.showEndTimePicker}>
+            <Text>יציאה</Text>
+          </Button>
+        </Container>
+
+        <Container style={[styles.section, { padding: 0, paddingTop: 0 }]}>
+          <Text style={styles.activities}>פעילויות</Text>
+          <Icon name="arrow-dropdown" />
+        </Container>
+        <FlatList data={activities} renderItem={({ item }) => <Text>{item.shortTitle}</Text>} />
+
+        <Container style={[styles.section, { padding: 0, paddingTop: 0 }]}>
+          <Text style={styles.activities}>הוסף פעילות</Text>
+          <Icon name="add-circle" />
+        </Container>
+
+        <Container style={[styles.section, { justifyContent: 'center' }]}>
+          <Button>
+            <Text>שמור</Text>
+          </Button>
+        </Container>
+        <DateTimePicker
+          mode="time"
+          isVisible={this.state.startTimePickerOpen}
+          onConfirm={this.handleStartTimePicked}
+          onCancel={this.hideStartTimePicker}
+        />
+        <DateTimePicker
+          mode="time"
+          isVisible={this.state.endTimePickerOpen}
+          onConfirm={this.handleEndTimePicked}
+          onCancel={this.hideEndTimePicker}
+        />
       </Container>
-    </Container>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
