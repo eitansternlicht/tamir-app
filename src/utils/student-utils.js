c200ebe8559b0bc4181a298b80a3a3ff40c6f0ef
Eitan Sternlicht <eitanoffun@gmail.com>
2019-04-28 01:53:12 +0300

    created student-utils.js to manage utility functions and constants related to student information and validation logic

---
+++ src/utils/student-utils.js
@@ -0,0 +1,44 @@
+const fieldTypes = {
+  PHONE: { validate: undefined, errorMsg: 'מספר פלאפון לא חוקי' },
+  SCHOOL_GRADE: {
+    valuesAnd: [
+      { valuesOr: ['יב', 'יא', 'י', 'ט', 'ח', 'ז', 'ו', 'ה', 'ד', 'ג', 'ב', 'א'] },
+      { valuesOr: [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+    ]
+  },
+  GENDER: { valuesOr: ['זכר', 'נקבה'] },
+  DATE: { validate: undefined, errorMsg: 'תאריך לא חוקי' },
+  GOV_ID: { validate: undefined, errorMsg: 'ת.ז. לא חוקית' },
+  EMAIL: { validate: undefined, errorMsg: 'כתובת אימייל לא חוקית' },
+  SHIRT_SIZE: { valuesOr: ['XS', 'S', 'M', 'L', 'XL', 'XXL'] }
+};
+
+const studentFirstFields = [
+  { name: 'שם משפחה' },
+  { name: 'שם פרטי' },
+  { name: 'קבוצות' },
+  { name: 'נייד', type: fieldTypes.PHONE },
+  { name: 'מין', type: fieldTypes.GENDER },
+  { name: 'כיתה', type: fieldTypes.SCHOOL_GRADE },
+  { name: 'מוסד לימודים' },
+  { name: 'כתובת' },
+  { name: 'שכונה' },
+  { name: 'תאריך לידה', type: fieldTypes.DATE },
+  { name: 'תעודת זהות', type: fieldTypes.GOV_ID },
+  { name: 'עיר' },
+  { name: 'מייל', type: fieldTypes.EMAIL },
+  { name: 'מידה חולצה', type: fieldTypes.SHIRT_SIZE },
+  { name: 'חברים' },
+  { name: 'מעגל חברתי' },
+  { name: 'תנועת נוער' },
+  { name: 'תחומי עניין' },
+  { name: 'בעיות מיוחדות' },
+  { name: 'ימים מועדפים' },
+  { name: 'איש צוות מטפל' }
+];
+
+const studentLastFields = [{ name: 'הערות' }];
+
+const studentFieldOrdering = studentFirstFields.concat(studentLastFields);
+
+export { studentFieldOrdering, fieldTypes };
