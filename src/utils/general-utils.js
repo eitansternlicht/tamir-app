38fc0fcc0b223ed67be75bed5508bd2ab9630b1c
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-07 22:21:42 +0300

    added addToEndIfDoesntExistAtEnd util function

--- src/utils/general-utils.js
+++ src/utils/general-utils.js
@@ -2,4 +2,10 @@ const entriesToObj = entries => Object.fromEntries(new Map(entries));
 
 const zip = (arr1, arr2) => arr1.map((elm, i) => [elm, arr2[i]]);
 
-export { entriesToObj, zip };
+const addToEndIfDoesntExistAtEnd = (item, items) => {
+  if (items.length === 0) return [item];
+  if (items[items.length - 1] === item) return items;
+  return [...items, item];
+};
+
+export { entriesToObj, zip, addToEndIfDoesntExistAtEnd };
