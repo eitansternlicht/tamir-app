c0dba5ab92597e26715d5da18d83866282e804c9
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-07 19:33:14 +0300

    refactored Fab, using react-navigation and sending data through react-navigation's params

--- src/scenes/StudentDetailsScene.js
+++ src/scenes/StudentDetailsScene.js
@@ -1,48 +1,26 @@
 import React from 'react';
-import { Container, Header, Left, Right, Body, Icon, Button, Title, Fab } from 'native-base';
+import { Container, Fab } from 'native-base';
 import MDIcon from 'react-native-vector-icons/MaterialIcons';
 import { StudentDetails } from '../components';
 import { getStudentName } from '../utils/student-utils';
 
-const student = {
-  sid: 'a',
-  'שם פרטי': 'חניך',
-  'שם משפחה': 'א׳',
-  נייד: '050-222-2222',
-  מין: 'ז',
-  כיתה: ['ג', 4],
-  'מוסד לימודים': 'בית ספר דוגמה',
-  כתובת: 'הרב שלום שבזי 3',
-  שכונה: 'נחלאות',
-  'תאריך לידה': new Date(2011, 9, 22),
-  'תעודת זהות': '204070635',
-  עיר: 'ירושלים',
-  מייל: 'student_A@gmail.com',
-  'מידה חולצה': 'S',
-  חברים: 'נעם, צחי, חיים, דני'
-};
-const StudentDetailsScene = () => (
-  <Container>
-    <Header>
-      <Left>
-        <Button transparent>
-          <Icon name="arrow-back" />
-        </Button>
-      </Left>
-      <Body>
-        <Title>{getStudentName(student)}</Title>
-      </Body>
-      <Right>
-        <Button transparent>
-          <Icon name="menu" />
-        </Button>
-      </Right>
-    </Header>
-    <Fab active style={{ backgroundColor: '#5067FF' }} position="bottomLeft">
-      <MDIcon name="edit" />
-    </Fab>
-    <StudentDetails student={student} />
-  </Container>
-);
+class StudentDetailsScene extends React.PureComponent {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: getStudentName(navigation.state.params.student)
+    };
+  };
+
+  render() {
+    return (
+      <Container>
+        <Fab active style={{ backgroundColor: '#5067FF' }} position="bottomLeft">
+          <MDIcon name="edit" />
+        </Fab>
+        <StudentDetails student={this.props.navigation.state.params.student} />
+      </Container>
+    );
+  }
+}
 
 export { StudentDetailsScene };
--- src/scenes/scenesections/StudentsTab.js
+++ src/scenes/scenesections/StudentsTab.js
@@ -1,79 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Icon, Fab, Button } from 'native-base';
-import MDIcon from 'react-native-vector-icons/MaterialIcons';
-import { FilterableList } from '../../components';
+import { FilterableList, Fab } from '../../components';
+import { groupsWithStudentDetails } from '../../utils/firebase-utils';
 
-const groups = {
-  groupA_UID: {
-    name: 'קבוצה א׳',
-    students: {
-      studentA_UID: { 'שם פרטי': 'חניך א׳' },
-      studentB_UID: { 'שם פרטי': 'חניך ב׳' },
-      studentC_UID: { 'שם פרטי': 'חניך ג׳' },
-      studentD_UID: { 'שם פרטי': 'חניך ד׳' },
-      studentE_UID: { 'שם פרטי': 'חניך ה׳' }
-    }
-  },
-  groupB_UID: {
-    name: 'קבוצה ב׳',
-    students: {
-      studentA_UID: { 'שם פרטי': 'חניך א׳' },
-      studentF_UID: { 'שם פרטי': 'חניך ו׳' },
-      studentG_UID: { 'שם פרטי': 'חניך ז׳' },
-      studentH_UID: { 'שם פרטי': 'חניך ח׳' },
-      studentI_UID: { 'שם פרטי': 'חניך ט׳' }
-    }
-  },
-  noGroup_UID: {
-    name: 'לא בקבוצה',
-    students: {
-      studentJ_UID: { 'שם פרטי': 'חניך י׳' },
-      studentK_UID: { 'שם פרטי': 'חניך יא׳' }
-    }
-  }
-};
-
-const INITIAL_STATE = {
-  fabMenuOpen: false
-};
-
-class StudentsTab extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = INITIAL_STATE;
-  }
-
-  renderFabIcon() {
-    return this.state.fabMenuOpen ? <Icon name="close" /> : <MDIcon name="edit" />;
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <FilterableList
-          withCategories
-          data={groups}
-          onPress={item => console.log('item pressed', item)}
-        />
-        <Fab
-          active={this.state.fabMenuOpen}
-          direction="up"
-          style={{ backgroundColor: '#5067FF' }}
-          position="bottomRight"
-          // eslint-disable-next-line react/no-access-state-in-setstate
-          onPress={() => this.setState({ fabMenuOpen: !this.state.fabMenuOpen })}>
-          {this.renderFabIcon()}
-          <Button style={{ backgroundColor: '#3B5998' }}>
-            <Icon name="people" />
-          </Button>
-          <Button style={{ backgroundColor: '#34A34F' }}>
-            <Icon name="person" />
-          </Button>
-        </Fab>
-      </View>
-    );
-  }
-}
+const StudentsTab = props => (
+  <View style={{ flex: 1 }}>
+    <FilterableList
+      withCategories
+      data={groupsWithStudentDetails(props.db)}
+      onPress={student => {
+        props.navigation.navigate('StudentDetailsScene', { student });
+      }}
+    />
+    <Fab
+      position="bottomLeft"
+      backgroundColor="#5067FF"
+      mdIcon="edit"
+      buttons={[
+        { onPress: undefined, backgroundColor: '#3B5998', name: 'people' },
+        { onPress: undefined, backgroundColor: '#34A34F', name: 'person' }
+      ]}
+    />
+  </View>
+);
 
 export { StudentsTab };
