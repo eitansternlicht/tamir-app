fd5ad1cdf83591efa7a41bc5c2a1981e1d1831ff
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-07 19:27:50 +0300

    removed NativeBase Header (using react-navigation for Header) added call to get entire Firebase DB and added a spinner to show while waiting for results

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Container, Header, Tabs, Tab, Title, Left, Right, Body, Icon, Button } from 'native-base';
+import { Container, Tabs, Tab, Spinner } from 'native-base';
 import { appName } from '../../app.json';
 import { AttendanceTab, StudentsTab } from './scenesections';
+import { initFirebase, getDB } from '../utils/firebase-utils';
 
-const MainScene = () => (
-  <Container>
-    <Header hasTabs>
-      <Left />
-      <Body>
-        <Title>{appName}</Title>
-      </Body>
-      <Right>
-        <Button transparent>
-          <Icon name="menu" />
-        </Button>
-      </Right>
-    </Header>
-    <Tabs>
-      <Tab heading="חניכים">
-        <StudentsTab />
-      </Tab>
-      <Tab heading="נוכחות">
-        <AttendanceTab />
-      </Tab>
-    </Tabs>
-  </Container>
-);
+class MainScene extends React.PureComponent {
+  static navigationOptions = {
+    title: appName
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+    initFirebase();
+    getDB().then(db => {
+      this.setState({ db, loading: false });
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.loading ? (
+          <Spinner />
+        ) : (
+          <Tabs>
+            <Tab heading="חניכים">
+              <StudentsTab db={this.state.db} navigation={this.props.navigation} />
+            </Tab>
+            <Tab heading="נוכחות">
+              <AttendanceTab db={this.state.db} navigation={this.props.navigation} />
+            </Tab>
+          </Tabs>
+        )}
+      </Container>
+    );
+  }
+}
 
 export { MainScene };
