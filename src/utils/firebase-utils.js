99b7744072db5bd7496238fc268f50843b1c57e0
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-07 19:23:31 +0300

    added groupsWithStudentDetails and getByNamespacedId helper functions

--- src/utils/firebase-utils.js
+++ src/utils/firebase-utils.js
@@ -1,7 +1,34 @@
 import firebase from 'firebase/app';
+import update from 'immutability-helper';
 import { entriesToObj, zip } from './general-utils';
 
 const COLLECTIONS = ['Activities', 'Groups', 'Students'];
+
+const groupsWithStudentDetails = db =>
+  update(db.Groups, {
+    $set: entriesToObj(
+      Object.keys(db.Groups).map(groupUID => [
+        groupUID,
+        update(db.Groups[groupUID], {
+          participants: {
+            $set: entriesToObj(
+              Object.keys(db.Groups[groupUID].participants).map(studentUID => [
+                studentUID,
+                update(db.Groups[groupUID].participants[studentUID], {
+                  $set: getByNamespacedId(studentUID, db)
+                })
+              ])
+            )
+          }
+        })
+      ])
+    )
+  });
+
+const getByNamespacedId = (namespacedId, db) => {
+  const [namespace, id] = splitNamespacedId(namespacedId);
+  return db[namespace] && db[namespace][id] ? db[namespace][id] : undefined;
+};
 
 const getDB = async () => {
   const promises = COLLECTIONS.map(collection =>
@@ -25,4 +52,13 @@ const initFirebase = () =>
     messagingSenderId: '463912944330'
   });
 
-export { getDB, initFirebase };
+const splitNamespacedId = namespacedId => {
+  for (let i = 0; i < namespacedId.length; i += 1) {
+    if (namespacedId[i] === '/') {
+      return [namespacedId.slice(0, i), namespacedId.slice(i + 1)];
+    }
+  }
+  return ['', namespacedId];
+};
+
+export { getDB, initFirebase, getByNamespacedId, groupsWithStudentDetails };
