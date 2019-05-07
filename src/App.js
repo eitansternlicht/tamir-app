be061ccc0f870544d97e39b2315e2c5baf5712ad
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-07 19:34:04 +0300

    created basic skeleton of adding react navigation to StudentsTab and StudentDetailsScene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 /* eslint-disable no-unused-vars */
-import firebase from 'firebase/app';
 import React from 'react';
-import { MainScene } from './scenes';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {
+  MainScene,
+  AttendanceCalendarScene,
+  StudentDetailsScene,
+  GroupParticipantsAttendanceScene,
+  GroupActivityDetailsScene,
+  PotentialStudentsScene,
+  SelectMultipleStudentsScene,
+  ManageGroupParticipantsScene,
+  ChooseStudentScene
+} from './scenes';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    firebase.initializeApp({
-      apiKey: 'AIzaSyDEaOcHtrnBB3GQ12z1nR9qRNfwEIsNrFo',
-      authDomain: 'tamir-db.firebaseapp.com',
-      databaseURL: 'https://tamir-db.firebaseio.com',
-      projectId: 'tamir-db',
-      storageBucket: 'tamir-db.appspot.com',
-      messagingSenderId: '463912944330'
-    });
-  }
+const AppContainer = createAppContainer(
+  createStackNavigator(
+    {
+      MainScene,
+      StudentDetailsScene
+    },
+    {
+      initialRouteName: 'MainScene'
+    }
+  )
+);
 
-  render() {
-    return <MainScene />;
-  }
-}
+const App = () => <AppContainer />;
 
 export default App;
