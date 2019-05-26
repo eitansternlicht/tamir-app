7f99e3a8ff06b9e9a4b2486a7f54504a19ea54b4
Eitan Sternlicht <eitanoffun@gmail.com>
2019-05-26 11:38:24 +0300

    added fix to AuthLoadingScene

--- src/scenes/AuthLoadingScene.js
+++ src/scenes/AuthLoadingScene.js
@@ -4,8 +4,9 @@ import { Container, Spinner } from 'native-base';
 class AuthLoadingScene extends React.Component {
   constructor(props) {
     super(props);
-    if (!props.loggedIn) props.navigation.navigate('LogInScene');
-    else props.navigation.navigate('MainScene'); // WON'T HAPPEN
+    if (!props.loggedIn) {
+      props.navigation.navigate('PhoneInputScene');
+    } else props.navigation.navigate('MainScene'); // WON'T HAPPEN
   }
 
   render() {
