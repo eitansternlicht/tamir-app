a134ad2d9c855732c83829fd90ac231dfe2c4b28
Eitan Sternlicht <eitanoffun@gmail.com>
2019-06-05 10:18:34 +0300

    refactored to data being db

--- src/scenes/ChooseStudentScene.js
+++ src/scenes/ChooseStudentScene.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from 'native-base';
 import { FilterableList } from '../components';
-import { groupsWithStudentDetails } from '../utils/firebase-utils';
 
 class ChooseStudentScene extends React.PureComponent {
   static navigationOptions = {
@@ -13,7 +12,7 @@ class ChooseStudentScene extends React.PureComponent {
       <Container>
         <FilterableList
           withCategories
-          data={groupsWithStudentDetails(this.props.navigation.state.params.db)}
+          data={this.props.navigation.state.params.db}
           onPress={student =>
             this.props.navigation.navigate('EditDiscussionDetailsScene', { student })
           }
