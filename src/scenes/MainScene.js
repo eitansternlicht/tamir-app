7007f37332aa955e41896bfd7e4a833c139ec366
Eitan Sternlicht <eitanoffun@gmail.com>
2019-06-05 10:25:55 +0300

    moved loading initial firebase db to MainScene, changing MainScene's internal state when results arrive, when ALL results arrive we stop Spinner and send full db to both tabs

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Container, Tabs, Tab, Button, Icon } from 'native-base';
+import { Container, Tabs, Tab, Button, Icon, Spinner } from 'native-base';
 import { appName } from '../../app.json';
 import { AttendanceTabScene, StudentsTabScene } from '.';
+import { firebase, readDB } from '../utils/firebase/firebase-db';
 
 class MainScene extends React.PureComponent {
   static navigationOptions = ({ navigation }) => {
@@ -16,23 +17,42 @@ class MainScene extends React.PureComponent {
     };
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {};
+    const tutorUID = firebase.auth().currentUser.uid;
+    readDB(tutorUID, this);
+  }
+  
   render() {
     return (
-      <Container>
-        <Tabs>
-          <Tab heading="חניכים">
-            <StudentsTabScene
-              navigation={this.props.navigation}
-              db={this.props.navigation.state.params.db}
-            />
-          </Tab>
-          <Tab heading="נוכחות">
-            <AttendanceTabScene
-              navigation={this.props.navigation}
-              db={this.props.navigation.state.params.db}
-            />
-          </Tab>
-        </Tabs>
+      <Container style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {!this.state.loadedAll ? (
+          <Spinner />
+        ) : (
+          <Tabs>
+            <Tab heading="חניכים">
+              <StudentsTabScene
+                navigation={this.props.navigation}
+                db={{
+                  AttendanceDays: this.state.AttendanceDays,
+                  Groups: this.state.Groups,
+                  Students: this.state.Students
+                }}
+              />
+            </Tab>
+            <Tab heading="נוכחות">
+              <AttendanceTabScene
+                navigation={this.props.navigation}
+                db={{
+                  AttendanceDays: this.state.AttendanceDays,
+                  Groups: this.state.Groups,
+                  Students: this.state.Students
+                }}
+              />
+            </Tab>
+          </Tabs>
+        )}
       </Container>
     );
   }
