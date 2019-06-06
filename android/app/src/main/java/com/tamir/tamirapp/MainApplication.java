f5e6594d91c013f9c9adb89e4dac6c4414ee8f6a
Eitan Sternlicht <eitanoffun@gmail.com>
2019-06-06 11:14:30 +0300

    installed Auth for android, removed react-native-gesture-handler

--- android/app/src/main/java/com/tamir/tamirapp/MainApplication.java
+++ android/app/src/main/java/com/tamir/tamirapp/MainApplication.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -31,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
-            new RNFirebaseFirestorePackage()
+            new RNFirebaseFirestorePackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
