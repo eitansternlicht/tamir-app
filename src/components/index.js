dbfe670b9c9725a86460ed66f38f374dbaa412cc
Eitan Sternlicht <eitanoffun@gmail.com>
2019-06-22 13:23:33 +0300

    removed StudentForm, FormikDatePicker

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,3 @@ export * from './StudentDetails';
 export * from './FilterableList';
 export * from './Fab';
 export * from './ShiftEditor';
-export * from './StudentForm';
-export * from './FormikDatePicker';
