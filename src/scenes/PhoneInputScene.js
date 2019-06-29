72f5c7da681875e5510a6099b2832efba9682ebf
Eitan Sternlicht <eitanoffun@gmail.com>
2019-06-29 19:10:08 +0300

    changed phoneauth to work on android

--- src/scenes/PhoneInputScene.js
+++ src/scenes/PhoneInputScene.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Text, Card, Content, Form, Button } from 'native-base';
+import { Container, Text, Card, Content, Form, Button, Platform } from 'native-base';
 import { Alert } from 'react-native';
 import PhoneInput from 'react-native-phone-input';
 import GlobalFont from 'react-native-global-font';
@@ -56,9 +56,10 @@ class PhoneInputScene extends React.Component {
               // TODO replace with this for phone auth
               if (this.phone.isValidNumber()) {
                 const phone = this.phone.getValue();
-                this.props.navigation.navigate('SmsCodeConfirmScene');
-                return;
-                // TODO: send request with phone (this.phone.getValue()) when not using emulator
+                if (Platform.OS === 'ios') {
+                  this.props.navigation.navigate('SmsCodeConfirmScene');
+                  return;
+                }
                 signInWithPhone(phone);
               }
               Alert.alert('Please enter a valid phone number ');
